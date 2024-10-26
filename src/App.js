b7afa07195f4e517a462564e9a0e958d77a430ee
Abdul Samea <46262404+abdulsamea@users.noreply.github.com>
2024-10-26 23:38:32 +0530

    fixed homepage url

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <div>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />}></Route>
+            <Route path="/anas-portfolio" element={<Home />}></Route>
             <Route path="*" element={<div>404 Not Found</div>}></Route>
           </Routes>
         </div>
